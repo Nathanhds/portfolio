1cef9e86e6bf171df2cc1687e5f8b6822d21f44a
Nathan Henrique <90628194+Nathanhds@users.noreply.github.com>
2023-09-05 21:16:36 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Experience from "./components/Experience";
 import Footer from "./components/Footer";
 import Introduction from "./components/Introduction";
 import Projects from "./components/Projects";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
