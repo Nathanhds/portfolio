0ca724eef178784b9da02f68ecb71f655454b697
Nathan Henrique <90628194+Nathanhds@users.noreply.github.com>
2023-09-08 12:00:01 -0300

    update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,11 @@
 export default function AboutMe() {
     return (
         <section className="about-me" id="about-me">
-            <img src="../dist/images/design-sem-nome-1.svg" alt="" className="nathan"/>
+            <img src="./dist/design-sem-nome-1.svg" alt="" className="nathan"/>
             <div className="text-about">
                 <p className="title">Sobre</p>
                 <p className="job">Sobre mim</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quos minus quo cupiditate laboriosam id autem, debitis delectus ullam necessitatibus magnam, nesciunt, exercitationem pariatur quasi earum placeat a ab ad voluptatibus veniam accusantium! Earum odit voluptatum nisi natus dolorem nulla ab similique doloribus! Excepturi voluptatem, porro sint rem similique modi.</p>
+                <p className="about-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quos minus quo cupiditate laboriosam id autem, debitis delectus ullam necessitatibus magnam, nesciunt, exercitationem pariatur quasi earum placeat a ab ad voluptatibus veniam accusantium! Earum odit voluptatum nisi natus dolorem nulla ab similique doloribus! Excepturi voluptatem, porro sint rem similique modi.</p>
             </div>
         </section>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 export default function Navbar() {
     return (
         <>
-            <header>
+            <header className="header">
                 <a href="#" className="logo">Nathan</a>
                 <nav>
                     <a href="#experience"className="link">Experiência</a> 
                     <a href="#about-me"className="link">Sobre Mim</a>
                     <a href="#projects"className="link">Portfólio</a>
-                    
                 </nav>
-                <button>Contato</button>
+                <a href="https://wa.me/43988160210?text=Olá%20Nathan" 
+               target="_blank" className="contact">Contato</a>
             </header>
             
         </>
